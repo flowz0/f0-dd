1d234627047c3528bb85cdb159ee6e6888ecbf37
flowz0 <billylflowers@gmail.com>
2025-06-25 18:26:55 -0700

    create getBlogById in blogRoutes

--- backend/src/routes/blogRoutes.js
+++ backend/src/routes/blogRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBlog, deleteBlog, getAllBlogs, updateBlog } from "../controllers/blogControllers.js";
+import { createBlog, deleteBlog, getAllBlogs, getBlogById, updateBlog } from "../controllers/blogControllers.js";
 
 const router = express.Router();
 
 router.get("/", getAllBlogs);
+router.get("/:id", getBlogById);
 router.post("/", createBlog);
 router.put("/:id", updateBlog);
 router.delete("/:id", deleteBlog);
