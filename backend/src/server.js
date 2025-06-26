37a16768ea50dda0fd743d748708e65dbd51c2cc
flowz0 <billylflowers@gmail.com>
2025-06-25 18:23:56 -0700

    implement dotenv and use express.json to parse bodies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,26 @@
 import express from "express";
 import blogRoutes from "./routes/blogRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
-app.use("/api/notes", blogRoutes);
+connectDB();
 
-app.listen(5001, () => {
-  console.log("Server started on PORT: 5001");
+// middleware
+app.use(express.json()); // parse JSON bodies: req.body
+
+// custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
+
+app.use("/api/blogs", blogRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server started on PORT:", PORT);
 });
