15b0800f65347a7ade58dba94fe468141810b8f3
flowz0 <billylflowers@gmail.com>
2025-06-25 21:38:05 -0700

    update getAllBlogs function and update blog._id in FeaturedBlogs component

--- frontend/app/components/pages/blog/FeaturedBlogs.tsx
+++ frontend/app/components/pages/blog/FeaturedBlogs.tsx
@@ -1,20 +1,20 @@
-import { getBlogs } from "@/lib/blogs";
+import { getAllBlogs } from "@/lib/blogs";
 import BlogCard from "./BlogCard";
 import { BlogProps } from "@/types/blog";
 
 export default async function FeaturedBlogs() {
-  const blogs = await getBlogs();
+  const blogs = await getAllBlogs();
 
   const featuredBlogs = blogs
     .filter((b): b is BlogProps => !!b.createdAt)
-    .sort((a,b) => new Date(b.createdAt!).getTime() - new Date(a.createdAt!).getTime())
+    .sort((a,b) => new Date(a.createdAt!).getTime() - new Date(b.createdAt!).getTime())
     .slice(0, 3);
 
   return (
     <section className="mt-4">
       <div className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
         {featuredBlogs.map((blog) => (
-          <BlogCard key={blog.id} blog={blog} />
+          <BlogCard key={blog._id} blog={blog} />
         ))}
       </div>
     </section>
